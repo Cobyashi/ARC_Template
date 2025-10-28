a8f924f216f805600f56a9600f9c346571f97b94
Joseph Dye <josephpdye@icloud.com>
2025-10-27 19:21:11 -0500

    Base Screen setup

--- src/screen.cpp
+++ src/screen.cpp
@@ -1,6 +1,6 @@
 #include "vex.h"
 
-Button::Button(int x, int y, int width, int height){
+Button::Button(int x, int y, int width=9, int height=2){
     this->x = x;
     this->y = y;
     this->width = width;
@@ -12,7 +12,7 @@ void Button::draw(vex::color backgroundColor, vex::color textColor, vex::fontTyp
     Brain.Screen.drawRectangle(x, y, width, height);
     Brain.Screen.setPenColor(textColor);
     Brain.Screen.setFont(fontSize);
-    Brain.Screen.setCursor(x+2, y+2);
+    Brain.Screen.setCursor(x+1, y+1);
     Brain.Screen.print(text);
 }
 
@@ -37,4 +37,19 @@ void Text::printText(){
     Brain.Screen.setFont(fontSize);
     Brain.Screen.setPenColor(textColor);
     Brain.Screen.print(words);
+}
+
+void createButtons(vex::color colors[8], std::string names[8], Button buttons[8]){
+    for(int i=0;i<4;i++){
+        buttons[i] = Button((3 + (9*i) + (2*i)), 3);
+    }
+    for(int i=4;i<8;i++){
+        buttons[i] = Button((3 + (9*i) + (2*i)), 6);
+    }
+    Button backButton = Button(3, 9);
+
+    for(int i=0;i<8;i++){
+        buttons[i].draw(colors[i], vex::color::white, vex::fontType::mono15, names[i]);
+    }
+    // backButton.draw(vex::color::COLOR, vex::color::white, vex::fontType::mono15, "Back");
 }
